34d65e43cd882f66057842396888ef83437bfc3b
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-27 11:58:49 +0200

    Correction exercice 6

--- array/app.js
+++ array/app.js
@@ -1,6 +1,11 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 var mois = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
 
+for (var i = 0; i < mois.length; i++) {
+	console.log(mois[i]);
+}
+
+
 array.push('Courgette');
 array[4].push('Citron');
 array.splice(3, 1);
