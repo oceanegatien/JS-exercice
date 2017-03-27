4c87ea2da2e1c9385219eddfd12742b4ba6dd798
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-27 15:33:20 +0200

    Function Exercice 4

--- functions/app.js
+++ functions/app.js
@@ -7,11 +7,26 @@ console.log(returnTrue());
 
 function hello(){
 	return 'Hello World'
+console.log(hello());
 }
-console.log(hello());*/
 
 function returnString(){
 	var string = "Hello" + " " + "World";
 	return string;
 };
-console.log(returnString());
+console.log(returnString());*/
+
+
+
+
+function nombre(a, b) {
+	if (a > b){
+	return "Le premier nombre est plus grand";
+} else if (a = b){
+	return "Les deux nombres sont identiques";
+} else{
+	return "Le premier nombre est plus petit";
+}};
+var result = nombre(36, 44);
+
+console.log(nombre());
