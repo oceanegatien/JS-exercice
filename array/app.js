641efc916e09863d49b5ec9524c47df0a59f5d23
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-27 10:17:07 +0200

    Array/Exercice5

--- array/app.js
+++ array/app.js
@@ -1,4 +1,4 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
-var mois = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre'];
+var mois = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
 
-console.log(mois[10]);
+console.log(mois[7]);
